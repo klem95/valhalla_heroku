6d5bcb8ae039dae864ed911855b9f7b1bbdc58e0
klem95 <enxbox@hotmail.com>
2021-10-09 21:09:18 +0200

    yas

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const request = require('request');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log("Trying to acess valhalla");
-  request('https://127.0.0.1:8002/', { json: true }, (err, res, body) => {
+  request('http://127.0.0.1:8002/', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   console.log(body.explanation);
   res.send(body.explanation);
