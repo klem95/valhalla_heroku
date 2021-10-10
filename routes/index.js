faeeb49c219cb18fe107315b5f750341808502d1
klem95 <enxbox@hotmail.com>
2021-10-10 14:03:34 +0200

    work

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/route', function(req, res, next) {
-  var url_parts = url.parse(req.url);
-  console.log(url_parts);
-  console.log(url_parts.pathname);
+  console.log("yo");
   //res.render('index', { title: 'Express' });
 });
 
